3ec665a7397674e337d41d02e7e19d1494e7c988
SonNns1709 <saywithme1709@gmail.com>
2025-05-10 20:14:45 +0700

    fix: bug Phone

--- Validation.py
+++ Validation.py
@@ -17,3 +17,13 @@ def inputFloatOrPass(value):
     except ValueError:
         print("Phải là số thực!");
         return inputFloatOrPass(value);
+
+@staticmethod
+def inputPhone(value):
+        s = input(value).strip()
+        if s == "":
+            return None
+        if not s.isdigit():
+            print("Số điện thoại chỉ được chứa chữ số!")
+            return Validation.inputPhone(value)
+        return s
